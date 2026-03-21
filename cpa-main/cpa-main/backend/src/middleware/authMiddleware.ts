3be960e1448908ce51786e5380679ebd0effefde
Eric <ericd.perin11@gmail.com>
2026-03-21 15:09:37 -0300

    feat: Add JWT authentication and authorization middleware with necessary dependencies.

--- cpa-main/cpa-main/backend/src/middleware/authMiddleware.ts
+++ cpa-main/cpa-main/backend/src/middleware/authMiddleware.ts
@@ -1,8 +1,9 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 require('dotenv').config(); // Carregar variáveis do .env
 import jwt from 'jsonwebtoken';
+import { UserResponseDTO } from '../dtos/AuthDTO';
 
-function authenticateToken(req: any, res: any, next: any) {
+function authenticateToken(req: Request, res: Response, next: NextFunction) {
     const authHeader = req.headers['authorization'];
     const token = authHeader && (authHeader as string).split(' ')[1];
 
@@ -10,28 +11,31 @@ function authenticateToken(req: any, res: any, next: any) {
         return res.sendStatus(401); // Token ausente
     }
 
-    jwt.verify(token, (process.env.SECRET_KEY as string), (err: any, user: any) => {
+    jwt.verify(token, (process.env.SECRET_KEY || 'secret'), (err: any, user: any) => {
         if (err) {
             if (err.name === 'TokenExpiredError') {
                 return res.status(401).json({ error: 'Sessão expirada. Faça login novamente.' });
             }
             return res.sendStatus(403); // Outro erro, como token inválido
         }
-        req.user = user;
+        req.user = user as UserResponseDTO & { role: string };
         next();
     });
 }
 
-function authorize(requiredPermissions: any) {
-    return (req: any, res: any, next: any) => {
-        const isAdmin = req.user.isAdmin || false;
+function authorize(requiredPermissions: string[]) {
+    return (req: Request, res: Response, next: NextFunction) => {
+        if (!req.user) return res.sendStatus(401);
+
+        const isAdmin = req.user.role === 'admin';
 
         if (isAdmin) {
             return next();
         }
 
-        const userPermissions = req.user.permissions || [];
-        const hasPermission = requiredPermissions.every((permission: any) =>
+        // Se houver permissões específicas futuramente
+        const userPermissions: string[] = (req.user as any).permissions || [];
+        const hasPermission = requiredPermissions.every((permission: string) =>
             userPermissions.includes(permission)
         );
 
@@ -43,7 +47,7 @@ function authorize(requiredPermissions: any) {
     };
 }
 
-export { 
+export {
     authenticateToken,
     authorize
- };
+};
